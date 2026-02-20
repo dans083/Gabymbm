281e0fe176515ee904865ba68c739a542a258e39
dans083 <danielcostasantos234@gmail.com>
2026-02-20 10:15:30 -0300

    Nova versão

--- script.js
+++ script.js
@@ -2,18 +2,36 @@ const envelope = document.getElementById("envelope");
 const carta = document.getElementById("carta");
 const fechar = document.getElementById("fechar");
 const musica = document.getElementById("musica");
+const coracoes = document.getElementById("coracoes");
+
+musica.volume = 0.3;
 
 envelope.addEventListener("click", () => {
-    envelope.classList.add("sumir");
 
-    musica.play().catch(() => {});
+    envelope.classList.add("abrir");
+
+    musica.play().catch(()=>{});
 
     setTimeout(() => {
         carta.style.display = "flex";
-    }, 500);
+        criarCoracoes();
+    }, 600);
 });
 
 fechar.addEventListener("click", () => {
     carta.style.display = "none";
-    envelope.classList.remove("sumir");
+    envelope.classList.remove("abrir");
+    coracoes.innerHTML = "";
 });
+
+function criarCoracoes() {
+    setInterval(() => {
+        const coracao = document.createElement("span");
+        coracao.innerHTML = "❤";
+        coracao.style.left = Math.random() * 100 + "vw";
+        coracao.style.animationDuration = (Math.random() * 3 + 2) + "s";
+        coracoes.appendChild(coracao);
+
+        setTimeout(() => coracao.remove(), 5000);
+    }, 300);
+}
